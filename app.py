82969a95f7a4709cdfb90f0586aae9c6e19266c6
Yanelis Serrano Taylor <yaneliserranotaylor@gmail.com>
2018-12-18 15:01:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import json
 
 data_path = Path(__file__).resolve().parent.parent / 'data'
 static_path = Path(__file__).resolve().parent / 'static' / 'dist'
-#print('data path: ' + str(data_path))
-#print('static path: ' + str(static_path))
-
 app = Flask(__name__, static_folder=static_path)
 
 @app.route("/")
